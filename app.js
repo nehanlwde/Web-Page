a1d0a525763655a91a0c1b855e01d826ae0094ee
nehanlwde <nehanlwde@gmail.com>
2025-04-04 20:12:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+Array.from(document.getElementsByTagName('input')).forEach((e , i)=>{
+    e.addEventListener('keyup', (el)=>{
+    if (e.value.length > 0) {
+        document.getElementsByClassName('bi-caret-down-fil')[i].style.transform = "rotate(180deg )";
+} else {
+   document.getElementsByClassName('bi-caret-down-fil')[i].style.transform = "rotate(0deg)";
+       }
+    })
+})
+
